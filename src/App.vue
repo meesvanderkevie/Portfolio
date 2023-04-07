ba2666c71a6745f9cc2eeaf0600699ca9991967e
Mees van der Kevie <mvanderkevie30@gmail.com>
2023-04-07 23:16:57 +0200

    .

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script>
+  import SocialBar from "./components/SocialBar.vue";
+  export default {
+    components: {
+      SocialBar
+    }
+  }
+
+</script>
+
+<template>
+  <div class="welkomsttekst">
+    <h1>Hallo, ik ben Mees van der Kevie</h1>
+    <p>Welkom bij mijn portfolio.</p>
+
+    <SocialBar />
+  </div>
+</template>
+
+<style scoped>
+  h1 {
+    font-size: 2rem;
+  }
+
+  p {
+    font-size: 1rem;
+  }
+
+  .welkomsttekst {
+    margin-top: 5rem;
+    text-align: center;
+    letter-spacing: 0.1rem;
+    line-height: 1rem;
+  }
+</style>
