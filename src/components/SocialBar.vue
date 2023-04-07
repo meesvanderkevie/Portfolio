d72f61f7954a43b60ce3c2731ff44584dbbcafb8
Mees van der Kevie <mvanderkevie30@gmail.com>
2023-04-07 21:17:06 +0200

    initial commit

---
+++ src/components/SocialBar.vue
@@ -0,0 +1,49 @@
+<script>
+    
+</script>
+
+<template>
+    <li>
+        <a href="https://github.com/meesvanderkevie">
+            <img src="../assets/images/github.svg" alt="github" />
+        </a>
+
+        <a href="https://instagram.com/meesvanderkevie">
+            <img src="../assets/images/instagram.svg" alt="instagram" />
+        </a>
+
+        <a href="https://twitter.com/ItzJustMees">
+            <img src="../assets/images/twitter.svg" alt="twitter" />
+        </a>
+
+        <a href="mailto:mvanderkevie30@gmail.com">
+            <img src="../assets/images/mail.svg" alt="mail" />
+        </a>
+
+
+    </li>
+</template>
+
+<style scoped>
+    li {
+        display: flex;
+        justify-content: center;
+        gap: 1rem;
+    }
+
+    a {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 3rem;
+        height: 3rem;
+        border-radius: 50%;
+        background-color: #f5f5f5;
+        box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.2);
+    }
+
+    img {
+        width: 2.5rem;
+        height: 2.5rem;
+    }
+</style>
