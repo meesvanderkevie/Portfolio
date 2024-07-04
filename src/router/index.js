54c5d3b0957bb13ac1f28c61d09d35db013a5448
Mees van der Kevie <mvanderkevie30@gmail.com>
2024-07-04 14:27:25 +0200

    added the pages to the router.

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,10 @@ import { createRouter, createWebHistory } from "vue-router";
 
 // Import views
 import Home from '@/views/Home.vue';
+import Opinie from '@/views/Opinie.vue';
+import Over from '@/views/Over.vue';
+import Samenvattingen from '@/views/Samenvattingen.vue';
+import Projecten from '@/views/Projecten.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -10,6 +14,26 @@ const router = createRouter({
             path: '/',
             name: 'Home',
             component: Home
+        },
+        {
+            path: '/opinie',
+            name: 'Opinie',
+            component: Opinie
+        },
+        {
+            path: '/over',
+            name: 'Over',
+            component: Over
+        },
+        {
+            path: '/samenvattingen',
+            name: 'Samenvattingen',
+            component: Samenvattingen
+        },
+        {
+            path: '/projecten',
+            name: 'Projecten',
+            component: Projecten
         }
     ]
 })
