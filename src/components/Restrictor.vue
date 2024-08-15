628e2a066728c511b587b7222cfab843f7dd171b
Mees van der Kevie <mvanderkevie30@gmail.com>
2024-08-15 22:55:47 +0200

    added some code to the added pages.

--- src/components/Restrictor.vue
+++ src/components/Restrictor.vue
@@ -1,13 +1,26 @@
 <template>
-    <div>
-        
+    <div id="wrapper">
+        <div id="restrictor" :style="`max-width: ${width}px`">
+            <slot></slot>
+        </div>
     </div>
 </template>
 <script>
 export default {
-    
-}
+    props: {
+        width: {
+            type: Number,
+            default: 800
+        },
+    },
+};
 </script>
 <style>
-    
+#wrapper {
+    display: flex;
+    justify-content: center;
+    #restrictor {
+        width: 100%;
+    }
+}
 </style>
---
+++ src/views/Article.vue
@@ -0,0 +1,52 @@
+<template>
+    <div id="article">
+
+    </div>
+</template>
+
+<script>
+import Restrictor from '@/components/Restrictor.vue';
+
+export default {
+    components: {
+        Restrictor,
+    },
+}
+</script>
+
+<style scope>
+#article {
+    text-align: left;
+
+    #article-banner {
+        width: calc(100vw);
+        height: 200px;
+        object-fit: cover;
+    }
+
+    #title {
+        text-align: center;
+        font-weight: bold;
+        font-size: x-large;
+        margin: 20px 0;
+    }
+
+    #content, #intro {
+        margin: 0 20px;
+        font-size: 18px;
+        word-spacing: 5px;
+    }
+
+    #intro {
+        margin-bottom: 25px;
+    }
+}
+
+#article-info {
+    color: gray;
+    margin-bottom: 8px;
+    .info-item {
+        margin-left: 20px;
+    }
+}
+</style>
