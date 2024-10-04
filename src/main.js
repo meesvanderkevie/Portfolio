bb779b85a8c4c6834d16f94de18941107b1af047
ItzJustMees <85079315+meesvanderkevie@users.noreply.github.com>
2024-10-04 10:07:39 +0000

    installed vue-meta package, added app.use in main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import './assets/main.css'
 import { createApp } from 'vue'
+import VueMeta from 'vue-meta'
 import App from './App.vue'
 import router from './router'
 import './assets/main.css'
 
 const app = createApp(App)
+app.use(VueMeta)
 app.use(router)
 app.mount('#app')
